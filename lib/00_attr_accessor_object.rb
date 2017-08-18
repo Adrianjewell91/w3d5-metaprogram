486fab7ace212923b3b53e00433124d2ec7f6c18
Adrian <adrianjewell91@gmail.com>
2017-08-18 10:31:19 -0700

    attr-accessor

--- lib/00_attr_accessor_object.rb
+++ lib/00_attr_accessor_object.rb
@@ -1,13 +1,15 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
     # ...
+    #Should be creating getter methods for instnace variables.
+
     names.each do |name|
       define_method("#{name}") do
-        instance_variable_get("#{name}")
+        instance_variable_get("@#{name}") #call the @ equivalent.
       end
 
       define_method("#{name}=") do |val|
-        instance_variable_set("#{name}=", val)
+        instance_variable_set("@#{name}", val) #don't stick the '=' sign in the setter.
       end
     end
   end
